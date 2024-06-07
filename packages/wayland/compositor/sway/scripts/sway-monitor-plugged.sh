20aa159ba19c1dede866a55b50be8e016909fc44
Benoit Coudour <benoit.coudour@gmail.com>
2024-06-08 00:47:20 +0200

    maybe we good now

--- packages/wayland/compositor/sway/scripts/sway-monitor-plugged.sh
+++ packages/wayland/compositor/sway/scripts/sway-monitor-plugged.sh
@@ -9,20 +9,21 @@ device="card0-HDMI-A-1"  # change as needed
 # and turn internal display off.
 if [ $(cat /sys/class/drm/${device}/status) == "connected" ];
 then 
-    systemctl stop essway.service                      # stop essway service
     swaymsg output "${internal}" disable               # turn internal display off
     swaymsg output "${external}" resolution 1920x1080  # set resolution
     swaymsg output "${external}" enable                # turn external display on
-    systemctl start essway.service
 
 # If external display was just physically disconnected, turn 
 # external display off and turn internal display on.
 elif [ $(cat /sys/class/drm/${device}/status) == "disconnected" ];
 then
-    systemctl stop essway.service                      # stop essway service
     swaymsg output "${external}" disable               # turn external display off
     swaymsg output "${internal}" enable                # turn internal display on
-    systemctl start essway.service
 else  # Do nothing if device status is unreadable
     exit
+fi
+
+# Restart essway.service if it is active
+if systemctl is-active --quiet essway.service; then
+    systemctl restart essway.service --force --force
 fi
